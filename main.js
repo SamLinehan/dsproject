5b4f7677923fe549309d424095492c134924946b
Samuel <Linehan@Samuels-MacBook-Air.local>
2015-11-16 15:18:15 -0700

    worked on search

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
     $.get('http://star-api.herokuapp.com/api/v1/stars.json', function(data){
       for(var i = 0; i < data.length; i ++){
-        console.log(data[i].label);
         if(data[i].label === $('#search').value){
-          console.log("Test");
+          console.log(data[i].label);
         }
       }
       var sortedList = data.sort(function(){
         return Math.round(Math.random()) - 0.5;
       })
-      
+
       for(var i = 0; i <= 10; i++){
         var randomList = document.getElementById('randomList');
         var newItem = document.createElement('li');
@@ -17,9 +16,11 @@ $(document).ready(function(){
         randomList.appendChild(newItem);
       }
     })
-    $.get('http://star-api.herokuapp.com/api/v1/exo_planets.json', function(data){
-      for(var i = 0; i < data.length; i++){
-        console.log(data[i].distance);
-      }
-    })
+    // $.get('http://star-api.herokuapp.com/api/v1/exo_planets.json', function(data){
+    //   for(var i = 0; i < data.length; i++){
+    //     if($('exoplanets').checked){
+    //       console.log("Checked");
+    //     }
+    //   }
+    // })
 })
